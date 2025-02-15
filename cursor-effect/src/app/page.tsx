636981e62bb05bc55a4f95f25bf0bb6d806ec70c
akshit Pathania <akshitpathania75@gmail.com>
2025-02-15 17:42:07 +0530

    grid view added

--- cursor-effect/src/app/page.tsx
+++ cursor-effect/src/app/page.tsx
@@ -11,7 +11,6 @@ import Gallery from "./componenets/Gallery";
 import Messages from "./componenets/Messages";
 import Timeline from "./componenets/Timeline";
 
-
 gsap.registerPlugin(ScrollToPlugin);
 
 const MouseScrollGrids = () => {
@@ -19,8 +18,10 @@ const MouseScrollGrids = () => {
   const cursorY = useRef(0);
   const targetX = useRef(0);
   const targetY = useRef(0);
+  const gridRef = useRef(null);
 
   useEffect(() => {
+
     const handleMouseMove = (event: MouseEvent) => {
       cursorX.current = event.clientX / window.innerWidth;
       cursorY.current = event.clientY / window.innerHeight;
@@ -53,13 +54,27 @@ const MouseScrollGrids = () => {
     smoothScroll();
   }, []);
 
-  return (
-   
+  useEffect(() => {
 
-  <div
+    gsap.fromTo(
+      gridRef.current,
+      { scale: 0.5, x: "-25%", y: "-25%" },
+      {
+        scale: 1,           
+        x: "0%",    
+        y: "0%",      
+        duration: 2,         
+        delay: 1,            
+        ease: "power2.out",  
+      }
+    );
+  }, []);
+
+  return (
+    <div
+      ref={gridRef}
       className="grid md:grid-cols-3 md:grid-rows-3 grid-cols-1 grid-rows-1 overflow-hidden bg-black"
       style={{
-        // background: "radial-gradient(circle, #00a3a3, #006666, black)",
         width: "200vw",
         height: "200vh",
         display: "grid",
@@ -68,16 +83,15 @@ const MouseScrollGrids = () => {
         gap: "5px",
       }}
     >
-      <Members/>
-      <Home/>
-      <Events/>
-      <Register/>
-      <Gallery/>
-     <Messages/>
-      <Timeline/>
+      <Members />
+      <Home />
+      <Events />
+      <Register />
+      <Gallery />
+      <Messages />
+      <Timeline />
       <div className='w-[50vw] h-[50vh]'>example</div>
     </div>
-
   );
 };
 
